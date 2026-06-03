256cc7f83ddcb945de75a78818f9e305979d8795
newtoncp2 <166084717+newtoncp2@users.noreply.github.com>
2026-06-03 12:29:33 +0000

    Style changes in 'lab9' - ex3.

--- inf1018/G1/lab9/foo.s
+++ inf1018/G1/lab9/foo.s
@@ -1,42 +1,27 @@
 .data
 .text
 .globl foo
+
 foo:
-
 movl $0, %r10d
-
 movl $0, %r9d
 
 loop:
-
 cmpl %esi, %r9d
-
 jge fim_loop
-
 movq %rdi, %r8
-
 movslq %r9d, %rax
-
 shlq $2, %rax
-
 addq %rax, %r8
-
 addl (%r8), %r10d
-
 cmpl $0, (%r8)
-
 jne fim_if
-
 movl %r10d, (%r8)
-
 movl $0, %r10d
 
 fim_if:
-
 addl $1, %r9d
-
 jmp loop
 
 fim_loop:
-
 ret 
